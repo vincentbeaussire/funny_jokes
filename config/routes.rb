a59678dc84252839b1577f618908bd88a007fc6b
Vincent Beaussire <vincentpolska@gmail.com>
2023-10-31 14:37:57 +0100

    joke controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "home", to: "home#index"
+  resources :jokes, only: [:show, :index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
